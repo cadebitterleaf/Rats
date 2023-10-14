16f99a1187aec3f644036443ebca79a599a1e32e
Flame48 <advait.s.rajan@gmail.com>
2023-10-14 18:46:29 -0500

    Added slopes and stair functionality
    The player can now go up and down stairs!

--- Rats!/Scripts/StairRegion.gd
+++ Rats!/Scripts/StairRegion.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+const StairAngleRatio = 2/9;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
--- Rats!/Scripts/player.gd
+++ Rats!/Scripts/player.gd
@@ -3,7 +3,8 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 
-# TODO: make player move up as they go left while they're in a stairRegion.
+# DM me for an explanation...
+var stairAngleRatio = 0;
 
 func _physics_process(delta):
 	
@@ -11,16 +12,48 @@ func _physics_process(delta):
 	var directionX = Input.get_axis("ui_left", "ui_right")
 	var directionY = Input.get_axis("ui_down", "ui_up")
 	
-	# If the user inputted a direction in the x axis [Left or Right Arrows], change velocities as such
-	if directionX:
-		velocity.x = directionX * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	var lastXDirection = 0;
 	
-	# Same as with X but for Y.
 	if directionY:
 		velocity.y = -directionY * SPEED
 	else:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
+	
+	
+	if directionX:
+		velocity.x = directionX * SPEED
+		velocity.y += -directionX * SPEED * stairAngleRatio
+		lastXDirection = directionX;
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	if (lastXDirection < 0):
+		get_node("AnimatedSprite2D").set_flip_h(false)
+	elif (lastXDirection > 0):
+		get_node("AnimatedSprite2D").set_flip_h(true)
+	
 
 	move_and_slide()
+
+func handleStairs(nodePath):
+	stairAngleRatio = get_node(nodePath).get_meta("StairAngleRatio")
+
+func _on_stair_region_body_entered(body):
+	if (body.name == "Player"):
+		handleStairs("/root/main/StairRegion")
+
+
+func _on_stair_region_body_exited(body):
+	if (body.name == "Player"):
+		stairAngleRatio = 0;
+
+
+
+func _on_stair_region_2_body_entered(body):
+	if (body.name == "Player"):
+		handleStairs("/root/main/StairRegion2")
+
+
+func _on_stair_region_2_body_exited(body):
+	if (body.name == "Player"):
+		stairAngleRatio = 0;
